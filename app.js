7f6c4d66a247cdc06f2a9e8d319fc2ba1e427aff
dave25uk <dave.brown04@live.com>
2026-04-13 15:04:38 +0100

    Initial Upload 3

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { createClient } from '@supabase/supabase-js';
 // Vite uses 'import.meta.env' instead of 'process.env'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
